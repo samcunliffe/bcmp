0e451a5e1aead419c40c4c7c672588b9a6b66d0f
Sam Cunliffe <sam.cunliffe@gmail.com>
2025-11-22 21:07:08 +0000

    Sketch of main processing.

--- bcmptidy.go
+++ bcmptidy.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/samcunliffe/bcmptidy/fileparser"
+)
 
 func main() {
-    fmt.Println("hello world")
+
+	input := os.Args[1:]
+	if len(input) != 1 {
+		fmt.Println("Usage: bcmptidy <zipfile>")
+		os.Exit(1)
+	}
+	fmt.Println("Input file: %s", input[0])
+	album, err := fileparser.ParseZipFileName(input[1])
+	if err != nil {
+		os.Exit(1)
+	}
+
+	// outputDirectory := fmt.Sprintf("%s/%s/%s", MUSICLIBRARYPATH, album.Artist, album.Title)
+	// mkdir(outputDirectory)
+	// zip.Extract(input)
+	fmt.Println("Hello, %s", album.Artist)
 }

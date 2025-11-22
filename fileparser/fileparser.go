866f4e762c80d2b8463ab9f01e955b8f0a39c0d9
Sam Cunliffe <sam.cunliffe@gmail.com>
2025-11-22 16:45:38 +0000

    String parsing.

--- fileparser/fileparser.go
+++ fileparser/fileparser.go
@@ -1,7 +1,23 @@
 package fileparser
 
-func ParseZipArchiveFileName(name string) (map[string]string, error) {
-	return nil, nil
+import (
+	"fmt"
+	"strings"
+)
+
+func ParseZipFileName(name string) (map[string]string, error) {
+	// Trim the .zip suffix
+	if !strings.HasSuffix(name, ".zip") {
+		return nil, fmt.Errorf("filename does not have .zip suffix")
+	}
+	name = strings.TrimSuffix(name, ".zip")
+
+	// Split into artist and album
+	if !strings.Contains(name, " - ") {
+		return nil, fmt.Errorf("filename does not contain ' - ' separator")
+	}
+	nameAndAlbum := strings.Split(name, " - ")
+	return map[string]string{"artist": nameAndAlbum[0], "album": nameAndAlbum[1]}, nil
 }
 
 func ParseMusicFileName(name string) (map[string]string, error) {

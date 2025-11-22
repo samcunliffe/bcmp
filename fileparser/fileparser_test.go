f143f27c241c06b652d44811b782738281f11d58
Sam Cunliffe <sam.cunliffe@gmail.com>
2025-11-22 21:13:04 +0000

    Add tests of the errors.

--- fileparser/fileparser_test.go
+++ fileparser/fileparser_test.go
@@ -2,7 +2,7 @@ package fileparser
 
 import "testing"
 
-var archiveFileTestCases = []struct {
+var testCases = []struct {
 	input_filename string
 	want_artist    string
 	want_album     string
@@ -22,8 +22,8 @@ var archiveFileTestCases = []struct {
 	{"Pallbearer - Mind Burns Alive (pre-order).zip", "Pallbearer", "Mind Burns Alive"},
 }
 
-func TestParseZipArchiveFileName(t *testing.T) {
-	for _, testcase := range archiveFileTestCases {
+func TestParseZipFileName(t *testing.T) {
+	for _, testcase := range testCases {
 		got, err := ParseZipFileName(testcase.input_filename)
 		if err != nil {
 			t.Errorf("ParseZipArchiveFileName(%q) returned error: %v", testcase.input_filename, err)
@@ -31,6 +31,21 @@ func TestParseZipArchiveFileName(t *testing.T) {
 		if got.Artist != testcase.want_artist || got.Title != testcase.want_album {
 			t.Errorf("ParseZipArchiveFileName(%q) = artist: %q, album: %q; want artist: %q, album: %q",
 				testcase.input_filename, got.Artist, got.Title, testcase.want_artist, testcase.want_album)
+		}
+	}
+}
+
+var errorTestCases = []string{
+	"NoHyphenHere.zip",
+	"MissingSuffix",
+	"Also Missing Hyphen.zip",
+}
+
+func TestParseZipFileNameErrors(t *testing.T) {
+	for _, filename := range errorTestCases {
+		_, err := ParseZipFileName(filename)
+		if err == nil {
+			t.Errorf("ParseZipArchiveFileName(%q) expected error but got nil", filename)
 		}
 	}
 }

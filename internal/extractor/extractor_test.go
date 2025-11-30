63fe960be0da1fd7a87d0520e7c86ad31e9ae751
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2025-11-30 14:18:37 +0000

    A few more test cases.

--- internal/extractor/extractor_test.go
+++ internal/extractor/extractor_test.go
@@ -3,6 +3,7 @@ package extractor
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -27,5 +28,31 @@ func TestExtractAndRename(t *testing.T) {
 		if _, err := os.Stat(wantPath); os.IsNotExist(err) {
 			t.Errorf("Expected file %s does not exist after extraction", wantPath)
 		}
+	}
+}
+
+func TestEmptyArchive(t *testing.T) {
+	destination := t.TempDir()
+	testfile := "testdata/empty.zip"
+
+	err := ExtractAndRename(testfile, destination)
+	if err == nil {
+		t.Errorf("Expected an error for an empty archive")
+	}
+	if !strings.Contains(err.Error(), "not a valid zip file") {
+		t.Errorf("Expected error message about empty archive, got: %v", err.Error())
+	}
+}
+
+func TestInvalidArchive(t *testing.T) {
+	destination := t.TempDir()
+	testfile := "testdata/one-file-no-music.zip"
+
+	err := ExtractAndRename(testfile, destination)
+	if err == nil {
+		t.Errorf("Expected an error for an invalid archive")
+	}
+	if !strings.Contains(err.Error(), "filename does not have a valid music file suffix") {
+		t.Errorf("Expected error message about no valid music files, got: %v", err.Error())
 	}
 }

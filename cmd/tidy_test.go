d889a326238e4110d7b6f315fcbbc506947d3cbb
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2025-12-03 09:32:28 +0000

    Oops. Logic fail.

--- cmd/tidy_test.go
+++ cmd/tidy_test.go
@@ -11,13 +11,13 @@ func TestTidyNoOp(t *testing.T) {
 
 	err := rootCmd.Execute()
 	if err == nil {
-		t.Errorf("Expected error for unimplemented tidy command, got nil")
+		t.Errorf("Expected error for tidy command, got nil")
 	}
 
 	want := "not implemented"
 	got := err.Error()
 
-	if strings.Contains(got, want) {
+	if !strings.Contains(got, want) {
 		t.Errorf("Expected error %q, got %q", want, got)
 	}
 }

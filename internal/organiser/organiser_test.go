8cb45d01f742c68ea2d87e569eb2c1b172825d7b
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2025-12-04 09:23:28 +0000

    Add tests of TrackDestination.

--- internal/organiser/organiser_test.go
+++ internal/organiser/organiser_test.go
@@ -111,3 +111,41 @@ func TestCheckFileValidMusicFile(t *testing.T) {
 		t.Errorf("CheckFile on valid music file returned error: %v", err)
 	}
 }
+
+func TestTrackDestinationPath(t *testing.T) {
+	track := parser.Track{
+		Number:    1,
+		Title:     "First Track",
+		FullTrack: "01 First Track",
+		FileType:  ".flac",
+	}
+	want := "01 First Track.flac"
+	if TrackDestinationPath(track, "./") != want {
+		t.Errorf("TrackDestinationPath() = %q; want %q", TrackDestinationPath(track, "./"), want)
+	}
+}
+
+func TestTrackDestinationPathWeirdPaths(t *testing.T) {
+	track := parser.Track{
+		Number:    1,
+		Title:     "First Track",
+		FullTrack: "01 First Track",
+		FileType:  ".flac",
+	}
+	testCases := []struct {
+		input string
+		want  string
+	}{
+		{"/a/b/c/../../", "/a/01 First Track.flac"},
+		{"a/b/c/../..", "a/01 First Track.flac"},
+		{"./", "01 First Track.flac"},
+		{"./music/", "music/01 First Track.flac"},
+	}
+
+	for _, testcase := range testCases {
+		got := TrackDestinationPath(track, testcase.input)
+		if got != testcase.want {
+			t.Errorf("TrackDestinationPath(_, %q) = %q; want %q", testcase.input, got, testcase.want)
+		}
+	}
+}

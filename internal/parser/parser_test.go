f874d392e3982974e0316937c0a43d2c4a2a28a9
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2025-12-07 16:45:24 +0000

    Separate out checking from parsing.

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -19,46 +19,6 @@ func TestParserConfig(t *testing.T) {
 	}
 }
 
-func TestIsCoverArtFile(t *testing.T) {
-	var testCases = []struct {
-		input string
-		want  bool
-	}{
-		{"cover.jpg", true},
-		{"COVER.PNG", true},
-		{"folder.jpg", true},
-		{"FOLDER.PNG", true},
-		{"not_cover.jpg", false},
-		{"flibble", false},
-		{"cover.jpeg", false},
-		{"some_other_file.mp3", false},
-		{"some_other_file.flac", false},
-	}
-	for _, testcase := range testCases {
-		got := IsCoverArtFile(testcase.input)
-		if got != testcase.want {
-			t.Errorf("IsCoverArtFile(%q) = %v; want %v", testcase.input, got, testcase.want)
-		}
-	}
-}
-
-func TestIsValidMusicFile(t *testing.T) {
-	var testCases = []struct {
-		input string
-		want  bool
-	}{
-		{"Crypta - Shades of Sorrow - 01 The Aftermath.txt", false}, // Invalid file extension
-		{"Crypta - Shades of Sorrow - 01 The Aftermath", false},     // No file extension
-		{"Crypta - Shades of Sorrow - 01 The Aftermath.mp3", true},  // Valid files
-		{"Crypta - Shades of Sorrow - 01 The Aftermath.flac", true},
-	}
-	for _, testcase := range testCases {
-		if IsValidMusicFile(testcase.input) != testcase.want {
-			t.Errorf("IsValidMusicFile(%q) = %v", testcase.input, !testcase.want)
-		}
-	}
-}
-
 func TestToTitleCase(t *testing.T) {
 	var testCases = []struct {
 		input string

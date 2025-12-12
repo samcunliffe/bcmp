f1a21ebcc6189d3a696d5d0a251c056f42565156
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2025-12-12 13:17:51 +0000

    More tests of the helper.

--- internal/bcmptest/bcmptest_test.go
+++ internal/bcmptest/bcmptest_test.go
@@ -1,7 +1,8 @@
 package bcmptest
 
 import (
-	"path"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,10 +10,29 @@ import (
 
 // Test the test helpers because testing all the way down 🐢🐢🐢
 func TestPutFileBack(t *testing.T) {
-	path := path.Join(t.TempDir(), "testfile.txt")
+	path := filepath.Join(t.TempDir(), "testfile.txt")
 	assert.NoFileExists(t, path, "Test setup error: test file %q already exists", path)
 
 	PutFileBack(t, path)
 
 	assert.FileExists(t, path, "PutFileBack did not create file %q", path)
+}
+
+func TestPutFileBackExistingFile(t *testing.T) {
+	path := "testdata/existingfile"
+	assert.FileExists(t, path, "Test setup error: test file %q does not exist", path)
+
+	PutFileBack(t, path)
+
+	assert.FileExists(t, path, "PutFileBack should not effect an existing file", path)
+}
+
+func TestPutFileBackNoPermissions(t *testing.T) {
+	tempdir := t.TempDir()
+	err := os.Chmod(tempdir, 0000)
+	assert.NoError(t, err, "Test setup error: failed to change permissions for %q: %v", tempdir, err)
+	defer os.Chmod(tempdir, 0644)
+
+	path := filepath.Join(tempdir, "no_permission_file.txt")
+	assert.Panics(t, func() { PutFileBack(t, path) })
 }

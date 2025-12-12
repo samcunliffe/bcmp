5cedbd0876dc6e2d636ea612f50dee7f89c409a0
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2025-12-12 12:18:29 +0000

    Premature generalisation.

--- internal/bcmptest/bcmptest.go
+++ internal/bcmptest/bcmptest.go
@@ -6,7 +6,8 @@ import (
 	"testing"
 )
 
-func putFileBack(t *testing.T, path string, empty bool) {
+// Utility test helper to recreate a moved file after testing is done.
+func PutFileBack(t *testing.T, path string) {
 	t.Helper()
 	f, err := os.Create(path)
 	if err != nil {
@@ -14,20 +15,7 @@ func putFileBack(t *testing.T, path string, empty bool) {
 	}
 	defer f.Close()
 
-	if !empty {
-		if _, err := f.WriteString("Just a non-empty test file."); err != nil {
-			panic("unable to write test file: " + err.Error())
-		}
+	if _, err := f.WriteString("Just a non-empty test file."); err != nil {
+		panic("unable to write test file: " + err.Error())
 	}
-}
-
-// Utility test helper to recreate a moved file after testing is done.
-func PutFileBack(t *testing.T, path string) {
-	t.Helper()
-	putFileBack(t, path, false)
-}
-
-func PutEmptyFileBack(t *testing.T, path string) {
-	t.Helper()
-	putFileBack(t, path, true)
 }

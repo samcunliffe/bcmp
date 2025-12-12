946e125cdd3bdcb09f86495c85ba98d0dff77caf
Sam Cunliffe <s.cunliffe@ucl.ac.uk>
2025-12-12 10:58:10 +0000

    Failure modes are tested in internals.

--- cmd/extract_test.go
+++ cmd/extract_test.go
@@ -9,35 +9,23 @@ import (
 
 func TestExtractEndToEndProcessing(t *testing.T) {
 	destination := t.TempDir()
+	source := "testdata/Artist - Album.zip"
 
-	testCases := []struct {
-		filename  string
-		wantError bool
-	}{
-		{"testdata/Artist - Album.zip", false},
-		{"testdata/Artist - Nonexistent Album.zip", true},
-	}
+	// Buffer to capture output
+	buf := &bytes.Buffer{}
+	rootCmd.SetOut(buf)
 
-	for _, tc := range testCases {
-		t.Run(tc.filename, func(t *testing.T) {
+	// Actually execute the command
+	rootCmd.SetArgs([]string{
+		"extract",
+		source,
+		"--destination",
+		destination,
+	})
+	defer rootCmd.SetArgs(nil)
+	err := rootCmd.Execute()
 
-			// Buffer to capture output
-			buf := &bytes.Buffer{}
-			rootCmd.SetOut(buf)
-
-			// Actually execute the command
-			rootCmd.SetArgs([]string{"extract", tc.filename, "--destination", destination})
-			err := rootCmd.Execute()
-			defer rootCmd.SetArgs(nil)
-
-			if tc.wantError {
-				assert.Error(t, err, "Expected error when processing %s", tc.filename)
-				return
-			}
-
-			got := buf.String()
-			assert.NoError(t, err, "Expected no error when processing %s", tc.filename)
-			assert.NotContains(t, got, "Error", "Expected no error messages in output for %s", tc.filename)
-		})
-	}
+	got := buf.String()
+	assert.NoError(t, err, "Expected no error when processing %s", source)
+	assert.NotContains(t, got, "Error", "Expected no error messages in output for %s", source)
 }
